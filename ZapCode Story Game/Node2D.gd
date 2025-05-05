1f6c2d202625e0672092fda6c5bc30c40866ef2a
andreamzp <andream169@nycstudents.net>
2025-05-05 14:25:17 -0400

    Add files via upload

---
+++ ZapCode Story Game/Node2D.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+onready var option_1 = $Button
+# Declare member variables here. Examples:
+# var a = 2
+func _ready():
+	pass
+
+func _on_Button_pressed():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
