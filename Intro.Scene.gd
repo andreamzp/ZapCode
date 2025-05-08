80594c624eacc3d06ea21e87fc13922c446f44ab
andreamzp <andream169@nycstudents.net>
2025-05-08 14:30:17 -0400

    Add files via upload

---
+++ Intro.Scene.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+func _ready():
+	yield(get_tree().create_timer(20,0),"timeout")
+	get_tree().change_scene("res://Node2D.tscnmm.tscn")
+
+
+
