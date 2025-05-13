41d44e078d630e7d425c0161484978f4acf15900
andreamzp <andream169@nycstudents.net>
2025-05-13 14:24:00 -0400

    Add files via upload

--- Button.gd
+++ Button.gd
@@ -1,16 +1,4 @@
 extends Button
 
-onready var option_1
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_button_pressed():
+	get_tree().change_scene("res://Node2D.tscnmm.tscn")
--- Intro.Scene.gd
+++ Intro.Scene.gd
@@ -4,7 +4,15 @@ extends Control
 # Declare member variables here. Examples:
 func _ready():
 	yield(get_tree().create_timer(20,0),"timeout")
+
+
+
+
+
+
+func _on_button_pressed():
 	get_tree().change_scene("res://Node2D.tscnmm.tscn")
 
-onready var start_game = $Panel/Button
 
+func _on_Button_pressed():
+	get_tree().change_scene("res://Node2D.tscnmm.tscn")
